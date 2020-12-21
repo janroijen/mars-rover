385de578717bc87adba184c166701941523ad5f7
Jan-Roijen <jan.roijen@shell.com>
2020-12-21 14:56:52 -0600

    Add first version

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,105 +1,146 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+// let store = {
+//   user: { name: "Student" },
+//   apod: "",
+//   rovers: ["Curiosity", "Opportunity", "Spirit"],
+//   selectedRover: "Curiosity",
+//   eventHandlers: [],
+// };
 
-// add our markup to the page
-const root = document.getElementById('root')
+let store = Immutable.Map({
+  rovers: Immutable.List(["Curiosity", "Opportunity", "Spirit"]),
+  selectedRover: "Curiosity",
+  eventHandlers: Immutable.List([]),
+});
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+// --- Workaround to add eventhandlers to components --------------------------
+
+const randomId = () => "id-" + Math.random().toString(36).slice(3);
+
+const addEventHandler = (id, event, handler) => {
+  store = store.merge({
+    eventHandlers: store.get("eventHandlers").push({ id, event, handler }),
+  });
+};
+const clearEventHandlers = () => {
+  store = store.merge({ eventHandlers: Immutable.List([]) });
+};
+
+const setEventHandlers = () => {
+  store.get("eventHandlers").forEach(({ id, event, handler }) => {
+    document.getElementById(id).addEventListener(event, handler);
+  });
+  clearEventHandlers();
+};
+
+// ----------------------------------------------------------------------------
+
+// Add our markup to the page
+const root = document.getElementById("root");
+
+const updateStore = (newState) => {
+  store = store.merge(newState);
+  render(root, store);
+};
 
 const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
+  root.innerHTML = App(state);
+  setEventHandlers();
+};
 
-
-// create content
-const App = (state) => {
-    let { rovers, apod } = state
-
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
-}
+// Code below will explicitly trigger rendering of the DOM on events.
+const rerender = () => render(root, store);
 
 // listening for load event because page should load before any JS is called
-window.addEventListener('load', () => {
-    render(root, store)
-})
+window.addEventListener("load", () => {
+  render(root, store);
+  getImages();
+});
 
-// ------------------------------------------------------  COMPONENTS
+// ---  COMPONENTS ------------------------------------------------------------
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
+// Create content
+const App = (state) => {
+  return `
+    ${Menu()}
+    ${Images(store.get("selectedRover"))}
+  `;
+};
 
-    return `
-        <h1>Hello!</h1>
-    `
-}
+const Button = ({ label, active, action }) => {
+  const id = randomId();
+  const button = `
+        <button
+            id="${id}" 
+            class="btn${active ? " active" : ""}"
+        >
+            ${label}
+        </button>
+    `;
+  addEventHandler(id, "click", action);
+  return button;
+};
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
+const Menu = () => {
+  const menu = `
+        <div class="menu">
+            ${store
+              .get("rovers")
+              .map((rover) =>
+                Button({
+                  label: rover,
+                  active: store.get("selectedRover") === rover,
+                  action: () => roverSelectionAction(rover),
+                })
+              )
+              .join("")}
+          </div>`;
+  return menu;
+};
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
+const roverSelectionAction = (rover) => {
+  store = store.set("selectedRover", rover);
+  rerender();
+};
 
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
+const Images = (rover) => {
+  if (!store.get("images")) {
+    return `<h1>Loading... </h1>`;
+  }
 
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
+  const roverInfo = store.getIn(["images", rover.toLowerCase()]);
+  return `
+    <div class="image-gallery">
+        ${RoverInfo(roverInfo)}
+        ${roverInfo
+          .get("photos")
+          .map((src) => Image({ src }))
+          .join("")}
+    </div>
+    `;
+};
 
-// ------------------------------------------------------  API CALLS
+const Image = ({ src }) => {
+  return `
+        <img class="image-box" src="${src}" />
+    `;
+};
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+const RoverInfo = (info) => {
+  return `
+        <div class="image-box">
+            <p><span class="label">Status:</span> ${info.get("status")}</p>
+            <p><span class="label">Launch:</span> ${info.get("launch_date")}</p>
+            <p><span class="label">Landing:</span> ${info.get("landing_date")}</p>
+            <p><span class="label">Photo:</span> ${info.get("photo_date")}</p>
+        </div>
+    `;
+};
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+// --- API CALL. Explicitly rerender DOM once images have been loaded ---------
 
-    return data
-}
+const getImages = () => {
+  fetch("http://localhost:3000/rovers")
+    .then((res) => res.json())
+    .then((images) => updateStore({ images }))
+    .then(() => rerender());
+};
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -1,28 +1,23 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const path = require("path");
 
-const app = express()
-const port = 3000
+const handlers = require("./handlers.js");
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const app = express();
+const port = 3000;
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// your API calls
+// Serving the web pages
+app.use("/", express.static(path.join(__dirname, "../public")));
 
-// example API call
-app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error:', err);
-    }
-})
+// Application API calls
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/rovers", handlers.handleRovers);
+app.get("/rovers/:rovername", handlers.handleRover);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
